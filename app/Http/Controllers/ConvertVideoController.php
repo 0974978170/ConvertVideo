e0757009bd7a15d6da53275b762a9dc4295985c9
manhkd <manhkd@trithucmoi.co>
2023-06-28 16:09:46 +0700

    Create function handle video

--- app/Http/Controllers/ConvertVideoController.php
+++ app/Http/Controllers/ConvertVideoController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ConvertVideoController extends Controller
 {
+    protected $exe;
+
+    public function __construct()
+    {
+        $this->exe = env('FFMPEG_PATH');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -19,7 +27,32 @@ class ConvertVideoController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->input());
+        $Ffmpeg = $this->exe . '\ffmpeg.exe';
+        $Data = $request->input();
+        $status = 200;
+        try {
+            if ($Data) {
+                foreach ($Data as $item) {
+                    $input_path = base_path('storage\app\public\input' . '\\' . $item['videoId']);
+                    $output_path = base_path('storage\app\public\output' . '\\' . $item['start'] . '_' . $item['duration'] . '.mp4');
+                    if (File::exists($input_path)) {
+
+                    } else {
+
+                    }
+                }
+                $response['Convert'] = 'Convert Success';
+            } else {
+                $response['error'] = 'ERROR';
+                $status = 500;
+            }
+        } catch (\Exception $e) {
+            $response['error'] = 'ERROR';
+            $status = 500;
+        } finally {
+            return response()->json(
+                [$response], $status);
+        }
     }
 
     /**
