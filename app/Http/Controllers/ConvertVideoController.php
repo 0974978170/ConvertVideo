2c466cd4eceb02cf2236fc03fe4c4be25c746b0c
manhkd <manhkd@trithucmoi.co>
2023-06-30 17:45:24 +0700

    Create function convert multi video

--- app/Http/Controllers/ConvertVideoController.php
+++ app/Http/Controllers/ConvertVideoController.php
@@ -25,8 +25,9 @@ class ConvertVideoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function convertVideo(Request $request)
     {
+        set_time_limit(0);
         $Ffmpeg = $this->exe . '\ffmpeg.exe';
         $Data = $request->input();
         $status = 200;
@@ -34,11 +35,11 @@ class ConvertVideoController extends Controller
             if ($Data) {
                 foreach ($Data as $item) {
                     $input_path = base_path('storage\app\public\input' . '\\' . $item['videoId']);
-                    $output_path = base_path('storage\app\public\output' . '\\' . $item['start'] . '_' . $item['duration'] . '.mp4');
+                    $array = explode('.',$item['videoId']);
+                    $output_path = base_path('storage\app\public\output' . '\\' . $array[0] . '_' . $item['start'] . '_' . $item['duration'] . '.mp4');
                     if (File::exists($input_path)) {
-
-                    } else {
-
+                        $Command = $Ffmpeg . ' -i ' . $input_path . ' -ss ' . $item['start'] . ' -t ' . $item['duration'] . ' -vf "scale=1920:1080" -c:v libx264 -preset fast -c:a pcm_s16le -f avi ' . $output_path;
+                        shell_exec($Command);
                     }
                 }
                 $response['Convert'] = 'Convert Success';
@@ -58,9 +59,41 @@ class ConvertVideoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function convertMultiVideo(Request $request)
     {
-        //
+        set_time_limit(0);
+        $Ffmpeg = $this->exe . '\ffmpeg.exe';
+        $Data = $request->input();
+        $status = 200;
+        try {
+            if ($Data) {
+                $cut_input_txt = base_path('storage\app\public\input\cut_input.txt');
+                $cut_input_file = fopen($cut_input_txt, 'w');
+                foreach ($Data as $item) {
+                    $input_path = base_path('storage\app\public\input' . '\\' . $item['videoId']);
+                    $array = explode('.',$item['videoId']);
+                    $output_path = base_path('storage\app\public\output' . '\\' . $array[0] . '_' . $item['start'] . '_' . $item['duration'] . '.mp4');
+                    if (File::exists($input_path)) {
+                        $Command = $Ffmpeg . ' -i ' . $input_path . ' -ss ' . $item['start'] . ' -t ' . $item['duration'] . ' -vf "scale=1920:1080" -c:v libx264 -preset fast -c:a pcm_s16le -f avi ' . $output_path;
+                        shell_exec($Command);
+                        fwrite($cut_input_file, "file '$output_path'" . PHP_EOL);
+                    }
+                }
+                $output_video_path = base_path('storage\app\public\output' . '\\' . 'output_video.mp4');
+                $Command = $Ffmpeg . '"' . ' -f concat -safe 0 -i ' . $cut_input_txt . ' -c:a pcm_s16le -f avi ' . '"' . $output_video_path;
+                shell_exec($Command);
+                $response['Convert'] = 'Convert Success';
+            } else {
+                $response['error'] = 'ERROR';
+                $status = 500;
+            }
+        } catch (\Exception $e) {
+            $response['error'] = 'ERROR';
+            $status = 500;
+        } finally {
+            return response()->json(
+                [$response], $status);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/convert', [ConvertVideoController::class, 'store']);
+Route::post('/convert', [ConvertVideoController::class, 'convertVideo']);
+Route::post('/convert-multi-video', [ConvertVideoController::class, 'convertMultiVideo']);
